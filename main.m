58aa1824f6d325c99845df21a50fd37f47605739
xiyudeng <52501352+xiyudeng@users.noreply.github.com>
2022-01-09 18:20:51 -0500

    Edit Rbar

--- main.m
+++ main.m
@@ -8,19 +8,18 @@ Nt = []; % list to store applicants number
 alpha = 0.001; % step size / learning rate
 c = 0.001; % interest rate
 % eta = % -1 or c
-% p = % probability to return the money with interest r
-% pie = % policy pi
 
-Rbar = 1; % Rbar
+Rbar = 0; % Rbar
 ninfo = 5; % number of information for each applicat (ninfo entries in s)
 
 % control parameters
 phi = 0.1*ones(ninfo,1);
 gamma = 0.1*ones(ninfo,1);
 z = [phi;gamma];
+R_cum = zeros([t,1]);
 
 
-%% 
+%%
 for i = 1:t
     % from environment
 
@@ -37,13 +36,14 @@ for i = 1:t
     info{1,i}.phi = phi;
     info{1,i}.gamma = gamma;
     info{1,i}.z = z;
-    
+
     theta1 = s * phi(:); % Nx1
     theta0 = s * gamma(:); % Nx1
 
-    pie = theta1 ./ (theta1+theta0);
+    % policy pi
+    pie = theta1 ./ (theta1+theta0); 
     info{1,i}.pie = pie;
-    
+
     % make decision
     A = zeros([N,1]);
     thre = 0.5; % threshold for lending
@@ -53,30 +53,67 @@ for i = 1:t
     info{1,i}.ratioA = sum(A,1)/N;
 
     % calculate return & get profit
-    p = returned_prob(s);
+    p = returned_prob(s);  % probability to return the money with interest r
     info{1,i}.p = p;
     R = zeros([N,1]);
     R(A == 1 & p > 0.5) = c;
     R(A == 1 & p <= 0.5) = -1;
     info{1,i}.R = R;
 
-%     phi, gamma = updateZ()
+    theta1 = s.*phi'; 
+    theta0 = s.*gamma'; 
+    exp_t1 = exp(theta1); exp_t1(exp_t1==0) = realmin;
+    exp_t0 = exp(theta0); exp_t0(exp_t0==0) = realmin;
+    exp_T1T0 = exp_t1 + exp_t0; exp_T1T0(exp_T1T0==0) = realmin;
+    exp_T1T0_sqr = exp_T1T0.^2; exp_T1T0_sqr(exp_T1T0_sqr==0) = realmin;
+    
+    Aid = A == 1; % index of the accepted applications
+    
+    % gradient of the probability
+    del_pi = zeros([N, 2*ninfo]);
+    del_pi(Aid,1:ninfo) = ...
+        ((exp_t1(Aid,:).*s(Aid,:).*exp_T1T0(Aid,:))...
+        + ((exp_t1(Aid,:).^2).*s(Aid,:)))...
+        ./exp_T1T0_sqr(Aid,:);
+    del_pi(~Aid,1:ninfo) = ...
+        (exp_t1(~Aid,:).*exp_t0(~Aid,:).*s(~Aid,:))...
+        ./ exp_T1T0_sqr(~Aid,:);
+    del_pi(~Aid,ninfo+1:end) = ...
+        ((exp_t0(~Aid,:).*s(~Aid,:).*exp_T1T0(~Aid,:))...
+        + ((exp_t0(~Aid,:).^2).*s(~Aid,:)))...
+        ./exp_T1T0_sqr(~Aid,:);
+    del_pi(Aid,ninfo+1:end) = ...
+        (exp_t1(Aid,:).*exp_t0(Aid,:).*s(Aid,:))...
+        ./ exp_T1T0_sqr(Aid,:);
+    
+    F = sum((R - Rbar).*(del_pi./pi),1);
+    
+    
+    % update paras for next iter
+    z = z + alpha.*F';
+    phi = z(1:ninfo);
+    gamma = z(ninfo+1:end);
+    Rbar = Rbar + sum(R)/(N*i);
+
+    R_cum(i) = sum(R);
 end
+
+plot(R_cum);
 
 % function  = probReturn()
 % % g(s_it) should be in [0, 1]
 % % P(eta = c) = p_it = h(s_it)
 % end
-% 
+%
 % function phi, gamma = updateZ()
-% 
+%
 % end
 
-
+%%
 function apcs_info = generate_apc_info(n_apcs)
-% generate_apc_info() is a function to generate random application. 
+% generate_apc_info() is a function to generate random application.
 %
-% The output apc_info is a vector containing the quantify information of 
+% The output apc_info is a vector containing the quantify information of
 % the application in the following order:
 %   [credit history, current income, living area, education level, existing
 %   debt]
@@ -85,35 +122,35 @@ function apcs_info = generate_apc_info(n_apcs)
 % want to be generated.
 
 %% credit history
-% The credit hisstory will be a random integer generated from a normal 
-% distribution with mean 650 and standard deviation 75. Here, the higher 
+% The credit hisstory will be a random integer generated from a normal
+% distribution with mean 650 and standard deviation 75. Here, the higher
 % the score indicates that the applicant has a better responsibility
 % toward his/her credit history thus has a higher probability that he/she
 % will honor his/her responsibility to pay the loan. The random number is
-% generated from a 
+% generated from a
 
 cr_hist = ceil(normrnd(650,75,[n_apcs,1]));
 
 %% current income
-% The current income will be a random number generated from a normal 
+% The current income will be a random number generated from a normal
 % distribution with mean 1 and standard deviation 0.5 (negative number will
 % be set to zero) which shows the current income of the applicant.
 
 incm = normrnd(1,0.5,[n_apcs,1]); incm(incm<0) = 0;
 
 %% living area
-% The living area will be a uniform random integer from 1 to 10 that 
+% The living area will be a uniform random integer from 1 to 10 that
 % indicates the location where the applicant is currently live.
 
 lvng_area = randi([1,10],[n_apcs,1]);
 
 %% education level
-% The education level will be a random integer from 1 to 4 generated from 
-% the right hand side of the normal distribution with 1 as the mean and 
-% standard deviation 1 that indicates the highest education level of the 
+% The education level will be a random integer from 1 to 4 generated from
+% the right hand side of the normal distribution with 1 as the mean and
+% standard deviation 1 that indicates the highest education level of the
 % applicant where:
 %   1 => no education until elementary school graduate
-%   2 => high school graduate 
+%   2 => high school graduate
 %   3 => college graduate
 %   4 => post-graduate school graduate
 % Any number higher than 4 will be converted to 4
@@ -121,9 +158,9 @@ lvng_area = randi([1,10],[n_apcs,1]);
 edu_lvl = ceil(abs(normrnd(1,1,[n_apcs,1]))); edu_lvl(edu_lvl > 4) = 4;
 
 %% existing debt
-% The existing debt will be a random number generated from a normal 
-% distribution with mean 0.5 and standard deviation 0.25 (negative number 
-% will be set to zero) which shows the current the debt that that the 
+% The existing debt will be a random number generated from a normal
+% distribution with mean 0.5 and standard deviation 0.25 (negative number
+% will be set to zero) which shows the current the debt that that the
 % applicant own before applying. The higher the
 % current debt will likely to be the higher risk.
 
@@ -135,6 +172,7 @@ apcs_info = [cr_hist, incm, lvng_area, edu_lvl, extng_debt];
 
 end
 
+%%
 function p = returned_prob(s)
 
 % point based on credit history
