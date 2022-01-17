d44e7dea7a63455de041ecf4966bdf756ef796db
xiyudeng <52501352+xiyudeng@users.noreply.github.com>
2022-01-17 18:54:22 -0500

    update codes for phi's upper bound

--- algo_v3.m
+++ algo_v3.m
@@ -7,27 +7,29 @@ info = cell([1 t]);
 Nt = []; % list to store applicants number
 alpha = 0.001; % step size / learning rate
 c = 0.1; % interest rate
-% eta = % -1 or c
 
 Rbar = 0; % Rbar
 ninfo = 1; % number of information for each applicat (ninfo entries in s)
 
 % control parameters
 phi = 0.1*ones([ninfo,1]);
-% gamma = 0.1*ones([ninfo,1]);
-% z = [phi;gamma];
-z = phi;
 phis = [];
+
 R_cum = zeros([t,1]);
 randR_cum = zeros([t,1]);
+R_avg = zeros([t,1]);
+randR_avg = zeros([t,1]);
 ratioAs = [];
+Rbars = Rbar;
+delpie = [];
+Ftests = [];
+Fs = [];
 
-%% for each period
+%% for each time step
 for i = 1:t
     % from environment
 
     % generate applicants number & info
-%     N = randi([500,1500],1); % Nt as random number in [500, 1500]
     N = 20000; %randi([10000,20000],1);
     Nt = [Nt,N];
     info{1,i}.Nt = N; % N applicants
@@ -40,8 +42,6 @@ for i = 1:t
     % calculate pie
     info{1,i}.phi = phi;
     phis = [phis;phi];
-%     info{1,i}.gamma = gamma;
-    info{1,i}.z = z;
 
     theta1 = s * phi(:); % Nx1
 
@@ -49,10 +49,10 @@ for i = 1:t
 
     % policy pi
 %     pie = exp(theta1) ./ (exp(theta1)+exp(theta0)); 
-    exp_t1 = exp(-theta1);
+    exp_t1 = exp(theta1);
         exp_t1(exp_t1==0) = realmin; exp_t1(isinf(exp_t1)) = realmax;
     
-    pie = 1./(1+exp_t1);
+    pie = exp_t1./(1+exp_t1); % problem here
     info{1,i}.pie = pie;
 
     % make decision
@@ -76,6 +76,8 @@ for i = 1:t
     R(A == 1 & return_varialbe >= p) = -1;
     info{1,i}.R = R;
 
+    A_1 = (p > (1/(1+c));
+
     % random choose action
     randAid = randsample([1:N],sum(A));
     randA = zeros(size(A));
@@ -87,15 +89,22 @@ for i = 1:t
     
     % index of the accepted applications
     Aid = find(A == 1); 
-    del_pi = partial_pi_partial_z(s,z,Aid,ninfo,N);
- 
-    del_pi(del_pi == 0) = realmin;
-    del_pi(del_pi == inf) = realmax;
-    pie(pie == 0) = realmin;
+    del_pi = partial_pi_partial_z(s,phi,Aid,ninfo,N);
+  
+%     del_pi(del_pi == 0) = realmin;
+%     del_pi(del_pi == inf) = realmax;
+%     pie(pie == 0) = realmin;
 
     Rbar = sum(R_cum)/sum(Nt);
-    F = ((R - Rbar).*(del_pi./pie))/N;
+%     Rbar = sum(R_cum)/i;
+
+    
+    F = ((R - Rbar).*del_pi./pie)/N;
+%     F = (mean(R - Rbar) * mean(del_pi./pie/N));   
+%     F = mean(F);
+
         Fs = sign(F); F = abs(F); F(isinf(F)) = realmax; F = Fs.*F;
+    
     % fix the  NaN problem
     if length(find(abs(F)>10^308))>(3*N/4)
         F = F./10^307;
@@ -107,49 +116,45 @@ for i = 1:t
         F = sum(F,1);
         F(isinf(F)) = realmax;
     end
-%     F = sum(F,1);
-%         F(isinf(F)) = realmax;
     
     
     % update paras for next 
-    z = z + alpha.*F';
-%     phi = z(1:ninfo);
-%     gamma = z(ninfo+1:end);
-    phi = z;
+    phi = phi + alpha.*F';
+    if phi > 1
+        phi = 1;
+    end
 
     R_cum(i) = sum(R);
     randR_cum(i) = sum(randR);
+    
+    R_avg(i) = R_cum(i)/N;
+    randR_avg(i) = randR_cum(i)/N;
 end
 
+%% plots
 figure(1)
-plot(R_cum);hold on
-plot(randR_cum,'r--')
+subplot(3,1,1);
+plot(R_avg);hold on
+plot(randR_avg,'r')
 xlabel('t');
 ylabel('cumR');
 title('rewards vs time')
 
 
-figure(2)
+% figure(2)
+% subplot(3,1,1);
+subplot(3,1,2);
 plot(phis);
 xlabel('t');
 ylabel('\phi');
 title('\phi vs time')
 
-figure(3)
+% figure(3)
+subplot(3,1,3);
 plot(ratioAs);
 xlabel('t');
 ylabel('ratioA');
 title('ratioA vs time')
-
-
-% function  = probReturn()
-% % g(s_it) should be in [0, 1]
-% % P(eta = c) = p_it = h(s_it)
-% end
-%
-% function phi, gamma = updateZ()
-%
-% end
 
 %%
 function apcs_info = random_apc_info(n_apcs)
@@ -169,41 +174,31 @@ function apcs_info = random_apc_info(n_apcs)
 end
 
 %% compute partial pi(probability) over partial z
-function del_pi = partial_pi_partial_z(s,z,Aid,ninfo,N)
-%     phi = z(1:ninfo);
-%     gamma = z(ninfo+1:end);
-    phi = z;
-%     del_pi = zeros([N, 2*ninfo]);
+% problem only positive
+function del_pi = partial_pi_partial_z(s,phi,Aid,ninfo,N)
     del_pi = zeros([N, ninfo]);
     
     tmp1 = s.*phi'; 
-%     tmp0 = s.*gamma'; 
     exp_t1 = exp(tmp1);
         exp_t1(exp_t1==0) = realmin; exp_t1(isinf(exp_t1)) = realmax;
-%     exp_t0 = exp(tmp0);
     exp_t0 = 1;
     exp_plus = exp_t1 + exp_t0;
     exp_sqr = exp_plus.^2;
         exp_sqr(exp_sqr==0) = realmin; exp_sqr(isinf(exp_sqr)) = realmax;
     
-%     del_pi(Aid,1:ninfo) = ((exp_t1(Aid,:).*s(Aid,:).*exp_plus(Aid,:))...
-%         + ((exp_t1(Aid,:).^2).*s(Aid,:)))./exp_sqr(Aid,:);
-%     del_pi(~Aid,1:ninfo) = (exp_t1(~Aid,:).*exp_t0(~Aid,:).*s(~Aid,:))...
-%         ./ exp_sqr(~Aid,:);
-%     del_pi(Aid,ninfo+1:end) = (exp_t1(Aid,:).*exp_t0(Aid,:).*s(Aid,:))...
-%         ./ exp_sqr(Aid,:);
-%     del_pi(~Aid,ninfo+1:end) = ((exp_t0(~Aid,:).*s(~Aid,:).*exp_plus(~Aid,:))...
-%         + ((exp_t0(~Aid,:).^2).*s(~Aid,:)))./exp_sqr(~Aid,:);
     del_pi(Aid,:) = (exp_t1(Aid,:).*s(Aid,:).*exp_plus(Aid,:) ...
         + (exp_t1(Aid,:).^2).*s(Aid,:))./exp_sqr(Aid,:);
 
     del_pi(~Aid,:) = (exp_t1(~Aid,:).*s(~Aid,:))...
         ./ exp_sqr(~Aid,:);
-        del_pi(del_pi==0) = realmin; del_pi(isinf(del_pi)) = realmax;
+
+%     del_pi(del_pi==0) = realmin; 
+%     del_pi(isinf(del_pi)) = realmax;
 end
 
 %%
 function p = returned_prob(s)
     % calculate the probability
     p = sum(s,2) ./ 4;
+%     p = sum(s,2);
 end
