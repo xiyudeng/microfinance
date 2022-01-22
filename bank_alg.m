6ec885a264823540d1d51d8f6f9c8be464f6fe71
xiyudeng <52501352+xiyudeng@users.noreply.github.com>
2022-01-22 14:54:37 -0500

    update also_bank: can work now

--- bank_alg.m
+++ bank_alg.m
@@ -1,6 +1,4 @@
-% standard algorithm bank
-
-function R_avg = bank_alg(N,t,c, ninfo)
+function R_avg = bank_alg(N,t,c, ninfo, nempty)
     phat = 1;
     R_cum = zeros([t,1]);
     R_avg = zeros([t,1]);
@@ -15,11 +13,13 @@ function R_avg = bank_alg(N,t,c, ninfo)
             % col ninfo+1: prob = #return / # approve
             % col ninfo+2: # return
             % col ninfo+3: # approve
-            s_database = [unique(s,'rows'), phat * ones(N,1), zeros(N,1), zeros((N,1))]; %initial all prob as phat
+            l = length(unique(s,'rows'));
+            s_database = [unique(s,'rows'), phat * ones(l,1), zeros(l,1), zeros(l,1)]; %initial all prob as phat
         else % i not 1
-            if (~ismember(unique(s,'rows'), s_database, 'rows')) % new entry
-                diff_s = setdiff(unique(s,'rows'),s_database); 
-                s_database = [diff_s, phat*ones(length(diff_s),1)]; % set prob as initial phat
+            diff_s = setdiff(unique(s,'rows'),s_database(:,1:ninfo),'rows'); 
+            % new entry: not all entries in s are in s_database
+            if (~isempty(diff_s)) 
+                s_database = [s_database; [diff_s, zeros(length(diff_s),1), zeros(length(diff_s),1),phat*ones(length(diff_s),1)]]; % set prob as initial phat
             end
         end
 
@@ -27,7 +27,9 @@ function R_avg = bank_alg(N,t,c, ninfo)
         % if prob >= 1/(1+c) : approve
         % if prob < 1/(1+c) : approve according to prob
 
-        idx_s = find(s_database(:,1:ninfo) == s);
+        
+        [~, idx_s] = ismember(s,s_database(:,1:ninfo),'rows');
+%         idx_s = find(s == s_database(:,1:ninfo));
         p = s_database(idx_s,ninfo+1);
 
         A = zeros(N,1); % initial A list as 0
@@ -47,14 +49,21 @@ function R_avg = bank_alg(N,t,c, ninfo)
         R_avg(i) = R_cum(i)/N;
 
         %% update the s_database with new prob
-        if i == 1
-            [~,~,ix] = unique(s,'rows');
-            count = accumarray(ix,1).'; % count
-        else
-        end
+        [~,~,ix] = unique(s,'rows');
+        approve_count = accumarray(ix,1); % count the number of approve
+        approve_idx = find(ismember(s_database(:,1:ninfo),unique(s,'rows'),'rows')==1);
 
+%         return_count = zeros(length(s_database),1);
+        s_return = s(R == 1+c,:);
+        [~,~,ix] = unique(s_return,'rows');
+        return_count = accumarray(ix,1); % count the number of approve
+        return_idx = find(ismember(s_database(:,1:ninfo),s_return,'rows')==1);
+%         s_return = [s_return;return_count]; % #col:ninfo+1
 
-        % df[df['score'] == 85]
+        s_database(return_idx,ninfo+2) = s_database(return_idx,ninfo+2)+return_count;
+        s_database(approve_idx,ninfo+3) = s_database(approve_idx,ninfo+3)+approve_count;
         
+        % update prob
+        s_database(:,ninfo+1) = s_database(:,ninfo+2)./s_database(:,ninfo+3);        
     end
 end
