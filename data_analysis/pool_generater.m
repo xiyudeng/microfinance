ed0a0aee0f464016a4badd57dc4eb1a78076cca6
xiyudeng <52501352+xiyudeng@users.noreply.github.com>
2022-04-25 20:42:57 -0400

    Update pool_generater.m

--- data_analysis/pool_generater.m
+++ data_analysis/pool_generater.m
@@ -2,6 +2,7 @@ function data_pool = pool_generater(pool_size, data_structure)
 indicator = rand(pool_size,9)*100;
 
 Percent = [0;cumsum(data_structure.G_languages.Percent)];
+% Percent = [0 80 82 83 88 92 100];
 languages = discretize(indicator(:,1),Percent);
 data_languages = cell2table(data_structure.G_languages.description_languages(languages),"VariableNames",["description_languages"]);
 
@@ -11,6 +12,7 @@ data_activity = cell2table(data_structure.G_activity_sector.activity(activity_se
 data_sector = cell2table(data_structure.G_activity_sector.sector(activity_sector),"VariableNames",["sector"]);
 
 Percent = [0;cumsum(data_structure.G_country_town.Percent)];
+
 country_town = discretize(indicator(:,3),Percent);
 data_country = cell2table(data_structure.G_country_town.location_country(country_town),"VariableNames",["location_country"]);
 data_town = cell2table(data_structure.G_country_town.location_town(country_town),"VariableNames",["location_town"]);
@@ -32,18 +34,21 @@ loss = discretize(indicator(:,7),Percent);
 data_loss= cell2table(data_structure.G_loss.terms_loss_liability_nonpayment(loss),"VariableNames",["terms_loss_liability_nonpayment"]);
 
 Percent = [0;cumsum(data_structure.G_exchange.Percent)];
+% disp(Percent);
+% Percent = [0, 30, 70, 100];
 exchange = discretize(indicator(:,8),Percent);
 data_exchange= cell2table(data_structure.G_exchange.terms_loss_liability_currency_exchange(exchange),"VariableNames",["terms_loss_liability_currency_exchange"]);
 
 Percent = [0;cumsum(data_structure.G_delinquent.Percent)];
+% Percent = [0, 50, 100];
 delinquent = discretize(indicator(:,9),Percent);
 data_delinquent= cell2table(data_structure.G_delinquent.delinquent(delinquent),"VariableNames",["delinquent"]);
 
-data_duration = array2table(normrnd(data_structure.duration_nor.mu, data_structure.duration_nor.sigma, pool_size,1),"VariableNames",["duration"]);
+data_duration = array2table(normrnd(data_structure.duration_nor.mu-150, data_structure.duration_nor.sigma, pool_size,1),"VariableNames",["duration"]);
 
-data_entries = array2table(normrnd(data_structure.entries_nor.mu, data_structure.entries_nor.sigma, pool_size,1),"VariableNames",["journal_totals_entries"]);
+data_entries = array2table(normrnd(data_structure.entries_nor.mu+2, data_structure.entries_nor.sigma+2, pool_size,1),"VariableNames",["journal_totals_entries"]);
 
-data_amount = array2table(normrnd(data_structure.amount_nor.mu, data_structure.amount_nor.sigma, pool_size,1),"VariableNames",["terms_loan_amount"]);
+data_amount = array2table(normrnd(data_structure.amount_nor.mu-300, data_structure.amount_nor.sigma+300, pool_size,1),"VariableNames",["terms_loan_amount"]);
 
 data_pool = [data_languages, data_activity, data_sector, data_country, ...
     data_town, data_gender, data_pictured, data_currency, data_loss, data_exchange, data_delinquent,data_entries,data_amount,data_duration];
